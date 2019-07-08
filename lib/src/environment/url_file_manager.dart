02cfa9fd2ed201f38b29eb6f5d851ed16a4ec79b
Todd Volkert <tvolkert@users.noreply.github.com>
2019-07-08 13:48:40 -0700

    Prepare for upcoming change to HttpRequest and HttpClientResponse (#23)
    An upcoming change to the Dart SDK will change `HttpRequest` and
    `HttpClientResponse` from implementing `Stream<List<int>>` to
    implementing `Stream<Uint8List>`.
    
    This forwards-compatible change prepares for that SDK breaking
    change by casting the Stream to `List<int>` before transforming
    it.
    
    https://github.com/dart-lang/sdk/issues/36900

--- lib/src/environment/url_file_manager.dart
+++ lib/src/environment/url_file_manager.dart
@@ -38,7 +38,7 @@ class UrlFileManager extends AbstractFileManager {
         .getUrl(Uri.parse(urlStr))
         .then((HttpClientRequest request) => request.close())
         .then((HttpClientResponse response) {
-          response.transform(utf8.decoder).listen((String contents) {
+          response.cast<List<int>>().transform(utf8.decoder).listen((String contents) {
             dataBuffer.write(contents);
           }, onDone: () {
             if (response.statusCode == 404) {
